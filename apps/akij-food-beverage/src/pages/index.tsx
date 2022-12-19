9bbeb4254eeb3eaf784bb0c0679bd3e8cedd2c0e
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-19 15:52:46 +0100

    refactor: adding new componenet folder

--- apps/akij-food-beverage/src/pages/index.tsx
+++ apps/akij-food-beverage/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Button } from "ui/Button";
+import { Button } from "ui";
 
 const Home: NextPage = () => {
   return (
