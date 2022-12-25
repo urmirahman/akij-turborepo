45c542182c49423860fbae5f4fe9ffac78a677c2
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-25 22:40:34 +0100

    feat: refactor

---
+++ apps/akij-food-beverage/src/components/layouts/Layout.test.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+
+import { ThemedContainer } from "../ui/elements/ThemedContainer";
+import { Layout } from "./Layout";
+
+jest.mock("../ui/elements/ThemedContainer", () => ({
+  ThemedContainer: jest.fn(() => null),
+}));
+
+describe("<Layout/>", () => {
+  it("can set theme", () => {
+    render(<Layout theme="primary-brown">CHILDREN</Layout>);
+
+    expect(ThemedContainer).toHaveBeenCalledWith(
+      expect.objectContaining({ theme: "primary-brown" }),
+      {}
+    );
+  });
+
+  it("has fallback to primary-brown theme", () => {
+    render(<Layout>CHILDREN</Layout>);
+
+    expect(ThemedContainer).toHaveBeenCalledWith(
+      expect.objectContaining({ theme: "primary-brown" }),
+      {}
+    );
+  });
+});
