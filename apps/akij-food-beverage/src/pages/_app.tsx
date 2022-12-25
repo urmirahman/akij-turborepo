dcb630397e9fe96cd020e063b24da6d521bfe99e
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-25 21:15:03 +0100

    feat: adding new icons and other relivent stuff

--- apps/akij-food-beverage/src/pages/_app.tsx
+++ apps/akij-food-beverage/src/pages/_app.tsx
@@ -3,19 +3,33 @@ import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import Error from "next/error";
 import { FormatImageUrlProvider } from "../components/ui/contexts/FormatImageUrlContext";
 import { baseTheme } from "../components/ui/foundations/theming";
-
+import { isErrorProps } from "../utils/isErrorProps";
+import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
-
+import { getLocaleInfo } from "../i18n/localeInfo";
 import "../styles/globals.css";
 import { formatImageUrl } from "../utils/formatImageUrl";
+import { useMemo } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const { locale } = useRouter();
+  const localeInfo = useMemo(() => getLocaleInfo(locale ?? ""), [locale]);
+
+  if (isErrorProps(pageProps)) {
+    return (
+      <Error
+        statusCode={pageProps.error.statusCode}
+        title={pageProps.error.message}
+      />
+    );
+  }
+
   return (
     <FormatImageUrlProvider value={formatImageUrl}>
       <MuiThemeProvider theme={baseTheme}>
