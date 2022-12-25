c57e4a4b2a9295dc2953646f85a5bcf325ec8dcd
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-25 21:43:10 +0100

    feat: update robot

--- apps/akij-food-beverage/src/pages/_app.tsx
+++ apps/akij-food-beverage/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import Error from "next/error";
+import Head from "next/head";
 import { FormatImageUrlProvider } from "../components/ui/contexts/FormatImageUrlContext";
 import { baseTheme } from "../components/ui/foundations/theming";
 import { isErrorProps } from "../utils/isErrorProps";
@@ -31,14 +32,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
   }
 
   return (
-    <FormatImageUrlProvider value={formatImageUrl}>
-      <MuiThemeProvider theme={baseTheme}>
-        <SessionProvider session={session}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </SessionProvider>
-      </MuiThemeProvider>
-    </FormatImageUrlProvider>
+    <>
+      <Head>
+        <meta content="initial-scale=1, width=device-width" name="viewport" />
+      </Head>
+      <FormatImageUrlProvider value={formatImageUrl}>
+        <MuiThemeProvider theme={baseTheme}>
+          <SessionProvider session={session}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </SessionProvider>
+        </MuiThemeProvider>
+      </FormatImageUrlProvider>
+    </>
   );
 };
 
