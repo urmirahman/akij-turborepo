47f7a1e6112bc6fbb2fc7fb792aa79e7d610607e
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-26 18:07:54 +0100

    feat: closeButton and IconButton

---
+++ apps/akij-food-beverage/src/components/ui/elements/IconButton/IconButton.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import { wrapper } from "../../../../testing";
+import type { IconButtonProps } from "./IconButton";
+import { IconButton } from "./IconButton";
+
+const props: IconButtonProps = {
+  title: "TITLE",
+  iconName: "search",
+  onClick: jest.fn(),
+};
+
+describe("<IconButton />", () => {
+  it("renders expected elements", () => {
+    const { container } = render(<IconButton {...props} />, { wrapper });
+
+    expect(screen.getByRole("button")).toHaveAttribute("title", "TITLE");
+    expect(container.querySelector("svg")).toBeInTheDocument();
+  });
+
+  it("calls onClick when clicked", () => {
+    const onClick = jest.fn();
+
+    render(<IconButton {...props} onClick={onClick} />, { wrapper });
+
+    fireEvent.click(screen.getByRole("button"));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
