87dacd2afea0c293c8815aaf8bf30c175aa71075
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-28 14:25:21 +0100

    refactor: refactor

--- apps/akij-food-beverage/src/components/ui/features/Hero/Products/Products.tsx
+++ apps/akij-food-beverage/src/components/ui/features/Hero/Products/Products.tsx
@@ -4,7 +4,7 @@ import AkijImage from "../Animations/dummy-assets/mojo.png";
 export default function Products() {
   return (
     <div>
-      <img src={AkijImage} alt="me" />
+      <img src={AkijImage.src} alt="me" />
     </div>
   );
 }
