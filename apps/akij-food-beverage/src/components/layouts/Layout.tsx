678409c0ac64ac54be65dbf1b7eed8c29e541b29
TAlam <tanvir.alam.shawn@gmail.com>
2023-01-01 23:20:49 +0100

    refactor: layout changes

--- apps/akij-food-beverage/src/components/layouts/Layout.tsx
+++ apps/akij-food-beverage/src/components/layouts/Layout.tsx
@@ -1,14 +1,6 @@
 import type { ReactNode } from "react";
-
-import { ContentContainer } from "../ui/elements/ContentContainer";
-import { GridColumn } from "../ui/elements/GridColumn";
-import { GridRow } from "../ui/elements/GridRow";
 import { ThemedContainer } from "../ui/elements/ThemedContainer";
-import { VisuallyHidden } from "../ui/elements/VisuallyHidden";
 import type { ThemeName } from "../ui/foundations/theming";
-/* import { ChatWindow } from "../../modules/ChatWindow"; */
-
-const SHOW_DEV_GRID_OVERLAY = false;
 
 type LayoutProps = {
   theme?: ThemeName;
@@ -28,57 +20,16 @@ export function Layout({
   header,
   children,
   footer,
-  h1,
 }: LayoutProps) {
   return (
     <>
-      {SHOW_DEV_GRID_OVERLAY && <GridOverlay />}
       <ThemedContainer theme={theme}>
         {header}
 
-        {h1 && <VisuallyHidden tag="h1">{h1}</VisuallyHidden>}
-
-        <main id="content">{children}</main>
+        <main>{children}</main>
 
         {footer}
       </ThemedContainer>
-      {/* {chat?.isOpen && (
-        <ChatWindow handleClose={chat.handleClose} src={chat.url} />
-      )} */}
     </>
-  );
-}
-
-function GridOverlay() {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex: 99999,
-        pointerEvents: "none",
-      }}
-    >
-      <ContentContainer>
-        <GridRow>
-          {Array(12)
-            .fill(null)
-            .map((_, i) => (
-              <GridColumn key={i} lg={1} xs={3}>
-                <div
-                  style={{
-                    background: "red",
-                    opacity: 0.3,
-                    height: "100vh",
-                  }}
-                />
-              </GridColumn>
-            ))}
-        </GridRow>
-      </ContentContainer>
-    </div>
   );
 }
