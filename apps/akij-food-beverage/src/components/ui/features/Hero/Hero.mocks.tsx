3c76dd60d021086c824ee9efdef28e8dd644e8e8
TAlam <tanvir.alam.shawn@gmail.com>
2023-01-06 01:07:18 +0100

    feat(;): deleted mock code

--- apps/akij-food-beverage/src/components/ui/features/Hero/Hero.mocks.tsx
+++ apps/akij-food-beverage/src/components/ui/features/Hero/Hero.mocks.tsx
@@ -8,9 +8,4 @@ export const HeroMock: HeroProps = {
     href: 'https://www.akijfood.com/public/assets/img/logo.png',
   },
   ingredients: true,
-  products: [
-    {
-      _id: 1,
-    },
-  ],
 }
