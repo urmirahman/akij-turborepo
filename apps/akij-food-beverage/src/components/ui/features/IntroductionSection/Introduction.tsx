5753b073abec4f8c48e41aea98325e42563f0a75
Abdullah Alnuman <alnuman@akijvgltd.onmicrosoft.com>
2023-01-09 15:46:41 +0600

    feat(features/introductionsection): 81 add introduction section in feature
    used tailwindcss, create introduction section

---
+++ apps/akij-food-beverage/src/components/ui/features/IntroductionSection/Introduction.tsx
@@ -0,0 +1,52 @@
+import {useEffect, useState} from 'react'
+import CheckmarkIcon from "ui/components/elements/Icon/checkmark";
+import { fetchProducts } from '../../../../utils/fetchProducts';
+import { urlForThumbnail } from './../../../../utils/imageProcess'
+const Introduction = () => {
+  const [state, setState] = useState({})
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const products = await fetchProducts()
+            setState(products[0])
+          } catch (e) {
+            console.log(e)
+          }
+        }
+        fetchData()
+      }, [])
+return (
+    <div className="customContainer gridTwo">
+        <div className='my-16 mx-auto'>
+           <img src={state.image && urlForThumbnail(state.image)} alt={state.name} height={400} width={300}/>
+        </div>
+        <div className='my-24 mx-auto'>
+            <h2 className="text-4xl font-semibold"> Introducing Our Juicy <span className= "brand">{state.brand}</span></h2>
+            <p className="description">{state.description}</p>
+            <ul className="space-y-4 sm:space-y-2">
+                <li className="space-y-2">
+                   <div className="flex items-center space-x-2">
+                      <CheckmarkIcon />
+                      <span>Review {state.numReviews}</span>
+                   </div>
+                </li>
+                <li className="space-y-2">
+                   <div className="flex items-center space-x-2">
+                     <CheckmarkIcon />
+                     <span>Rating {state.rating}</span>
+                   </div>
+                </li>
+                <li className="space-y-2">
+                   <div className="flex items-center space-x-2">
+                     <CheckmarkIcon />
+                     <span>Price {state.price}</span>
+                    </div>
+                </li>
+            </ul>
+          <button className="mt-4 outlineBtn">Buy Now</button> 
+        </div>
+    </div>
+  )
+}
+export default Introduction;
+
--- apps/akij-food-beverage/src/pages/index.tsx
+++ apps/akij-food-beverage/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Layout } from '~/components/layouts'
 import { Footer } from '../components/ui/features/Footer'
 import { Header } from '../components/ui/features/Header'
 import { Hero } from '../components/ui/features/Hero'
+import IntroductionSection  from '~/components/ui/features/IntroductionSection/Introduction'
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
           <Hero headingLine={'Tanvir'} ingredients={true} />
         </main>
       </Layout>
+      <IntroductionSection />
     </>
   )
 }
