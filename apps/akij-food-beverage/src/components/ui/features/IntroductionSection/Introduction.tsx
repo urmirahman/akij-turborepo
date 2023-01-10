ddfb424d290a2702b6b8a6941eabb46dec082250
Abdullah Alnuman <alnuman@akijvgltd.onmicrosoft.com>
2023-01-10 13:01:32 +0600

    feat(introductionsection): add left and right arrow and action onclick

--- apps/akij-food-beverage/src/components/ui/features/IntroductionSection/Introduction.tsx
+++ apps/akij-food-beverage/src/components/ui/features/IntroductionSection/Introduction.tsx
@@ -1,9 +1,10 @@
 import CheckmarkIcon from "ui/components/elements/Icon/checkmark";
+import Arrow from "ui/components/elements/Icon/arrow";
 import { urlForThumbnail } from './../../../../utils/imageProcess';
-import { Alert, CircularProgress} from '@mui/material'
+import { Alert, CircularProgress} from '@mui/material';
 import useFetch from "./useFetch";
 const Introduction = () => {
-  const {product,loading,error} = useFetch();
+  const {products,loading,error,currentIndex,nextProduct,prevProduct} = useFetch();
 return (
 <>
   {loading ? (
@@ -11,34 +12,38 @@ return (
     ) : error ? (
        <Alert variant="standard">{error}</Alert>
     ) : (
-    <div className="customContainer gridTwo">
-        <div className='my-16 mx-auto px-8'>
-           <img src={product.image && urlForThumbnail(product.image)} alt={product.name} height={400} width={300}/>
+    <div className="customContainer gridTwo my-20">
+        <div className='my-16 mx-auto px-8 customflex sm:w-80 '>
+          <button className="rotate-180" onClick={prevProduct}><Arrow /></button>
+           <img src={products[currentIndex].image && urlForThumbnail(products[currentIndex].image)} alt={products[currentIndex].name} height={400} width={230}/>
+           <button onClick={nextProduct}><Arrow /></button>
         </div>
-        <div className='my-24 mx-auto px-8'>
-            <h2 className="text-4xl font-semibold"> Introducing Our Juicy <span className= "brand">{product.brand}</span></h2>
-            <p className="description">{product.description}</p>
+        <div className='my-24 mx-auto px-8 customflex sm:w-80'>
+        <div>
+            <h2 className="text-4xl font-semibold"> Introducing Our Juicy <span className= "brand">{products[currentIndex].brand}</span></h2>
+            <p className="description">{products[currentIndex].description}</p>
             <ul className="space-y-4 sm:space-y-2">
                 <li className="space-y-2">
-                   <div className="flex items-center space-x-2">
+                   <div className="customflex">
                       <CheckmarkIcon />
-                      <span>Review {product.numReviews}</span>
+                      <span>Review {products[currentIndex].numReviews}</span>
                    </div>
                 </li>
                 <li className="space-y-2">
-                   <div className="flex items-center space-x-2">
+                   <div className="customflex">
                      <CheckmarkIcon />
-                     <span>Rating {product.rating}</span>
+                     <span>Rating {products[currentIndex].rating}</span>
                    </div>
                 </li>
                 <li className="space-y-2">
-                   <div className="flex items-center space-x-2">
+                   <div className="customflex">
                      <CheckmarkIcon />
-                     <span>Price {product.price}</span>
+                     <span>Price {products[currentIndex].price}</span>
                     </div>
                 </li>
             </ul>
           <button className="mt-4 outlineBtn">Buy Now</button> 
+          </div>
         </div>
     </div>
     )
--- apps/akij-food-beverage/src/components/ui/features/IntroductionSection/useFetch.ts
+++ apps/akij-food-beverage/src/components/ui/features/IntroductionSection/useFetch.ts
@@ -1,28 +1,33 @@
 import {useEffect, useState} from 'react';
 import { fetchProducts } from '../../../../utils/fetchProducts';
-import { CmsProducts } from '~/types/types';
-
 
 const useFetch = () => {
 const [state, setState] = useState({
-        product: {},
+        products: [],
         error: '',
         loading: true,
+        currentIndex: 0,
      })    
       useEffect(() => {
         const fetchData = async () => {
-          try {
-            const products = await fetchProducts();
-            const product : CmsProducts = products[0];
-            setState({ product, loading: false, error: '' })
-          } catch (e) {
-            setState({ product, loading: false, error: e.message })
+            try {
+              const products = await fetchProducts()
+              setState({ products, loading: false, error: '',currentIndex: 0 })
+            } catch (e) {
+              setState({ products, loading: false, error: e.message, currentIndex: 0 })
+            }
           }
-        }
-        fetchData()
+          fetchData()
       }, [])
-  const { loading, error, product } = state;
-  return {loading, error, product};
+  const nextProduct = () =>{
+     const newcurrentIndex = (state.currentIndex+1) % state.products.length;
+     setState({...state, currentIndex: newcurrentIndex})
+  }
+  const prevProduct = () =>{
+    const newcurrentIndex = currentIndex-1 < 0 ? products.length -1 : currentIndex-1;
+    setState({...state, currentIndex: newcurrentIndex})
+  }    
+  const { loading, error, products, currentIndex } = state;
+  return {loading, error, products, currentIndex,nextProduct,prevProduct};
 }
-
 export default useFetch;
